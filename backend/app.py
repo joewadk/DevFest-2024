4708d47ddc7ea74826b0d4d84e9319667a08337a
IshmamF <ishmamf2003@gmail.com>
2024-02-03 15:47:11 -0500

    feat: use google maps api and civic info api

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,25 @@
 from flask import Flask
+from dotenv import load_dotenv
+import requests
+from geopy.geocoders import GoogleV3
+import os
 from datetime import datetime
 app= Flask(__name__)
+
+load_dotenv()
+GOOGLE_CLOUD_API = os.getenv('GOOGLE_CLOUD_API')
+geolocator = GoogleV3(api_key=GOOGLE_CLOUD_API)
+
+user_input=input("Upload Zip Code ")
+location = geolocator.geocode(user_input)
+
+print(f"latitude : {location.latitude} longitude : {location.longitude}")
+
+API_URL = "https://civicinfo.googleapis.com/civicinfo/v2/representatives?address="
+address = "11203"
+
+query = API_URL + address +  "&key=" + GOOGLE_CLOUD_API
+
+response = requests.get(query).json()
+
+#print(response)
