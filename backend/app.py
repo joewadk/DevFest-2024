73088c884650bbd03b09c8048f75f6c93ef518c2
IshmamF <ishmamf2003@gmail.com>
2024-02-03 22:11:24 -0500

    wrapper

--- backend/app.py
+++ backend/app.py
@@ -7,31 +7,22 @@ import os
 #import openai
 import json
 import requests
-
+app=Flask(__name__)
 load_dotenv()
-# geolocator = GoogleV3(api_key='AIzaSyDfCBbLpQ4gA5irk6up5IpvC2XNXOECoi4')
-# location = geolocator.geocode(user_input)
+geolocator = GoogleV3(api_key='AIzaSyDfCBbLpQ4gA5irk6up5IpvC2XNXOECoi4')
 
 #openai.api_key = os.getenv("GPT_KEY")
 GOOGLE_CLOUD_API = os.getenv('GOOGLE_CLOUD_API')
 geolocator = GoogleV3(api_key=GOOGLE_CLOUD_API)
 
-# print(f"latitude : {location.latitude} longitude : {location.longitude}")
-
-API_URL = "https://civicinfo.googleapis.com/civicinfo/v2/representatives?address="
-address = "11203"
-
-# query = API_URL + address +  "&key=" + GOOGLE_CLOUD_API
-
-# response = requests.get(query).json()
-
-# print(response)
 startDest = geolocator.geocode(input("Start "))
 endDest = geolocator.geocode(input("End "))
+weightItem = input("State the weight of your item ")
 
 startCoords = (startDest.latitude, startDest.longitude)
 endCoords = (endDest.latitude, endDest.longitude)
 distance = geodesic(startCoords, endCoords).miles
+distance_str = str(distance)
 
 CARBON_KEY = os.getenv('CARBON_KEY')
 
@@ -44,7 +35,7 @@ headers = {
 
 data = {
     "type": "shipping",
-    "weight_value": 100,
+    "weight_value": int(weightItem),
     "weight_unit": "lb",
     "distance_value": distance,
     "distance_unit": "mi",
@@ -52,7 +43,8 @@ data = {
 }
 
 response = requests.post(url, headers=headers, data=json.dumps(data))
-print(response.json()['data']['attributes']['carbon_lb'])
+carbon_fp = response.json()['data']['attributes']['carbon_lb']
+print(carbon_fp)
 
 # carbon_url= 'https://www.carboninterface.com/api/v1/estimates'
 # carbon_headers ={ 'Authorization': 'Bearer kl1r8VkxPP2a6t3qw1cBQ',
@@ -72,7 +64,12 @@ print(response.json()['data']['attributes']['carbon_lb'])
 def func(zipcode):
     return zipcode
 
-@app.route('/')
-def hello():
-    return "wassup"
+@app.route('/distance')
+def distance():
+    return distance_str
+
+@app.route('/carbon_emission')
+def carbon_weight():
+    return str(carbon_fp)
+
 app.run(debug=False)
