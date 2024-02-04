a77b810f5bd8a1654ff591983abd81feaaa7f7ca
ShehabMohsen <aquashehab612@gmail.com>
2024-02-04 01:05:33 -0500

    feat: reorganize carbon emission

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from tkinter import N
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 from geopy.geocoders import GoogleV3
 from flask_cors import CORS
@@ -11,23 +11,12 @@ import requests
 app=Flask(__name__)
 CORS(app)
 load_dotenv()
-geolocator = GoogleV3(api_key='AIzaSyDfCBbLpQ4gA5irk6up5IpvC2XNXOECoi4')
 
 #openai.api_key = os.getenv("GPT_KEY")
 GOOGLE_CLOUD_API = os.getenv('GOOGLE_CLOUD_API')
 geolocator = GoogleV3(api_key=GOOGLE_CLOUD_API)
 
-startDest = geolocator.geocode(input("Start "))
-endDest = geolocator.geocode(input("End "))
-weightItem = input("State the weight of your item ")
-
-startCoords = (startDest.latitude, startDest.longitude)
-endCoords = (endDest.latitude, endDest.longitude)
-distance = geodesic(startCoords, endCoords).miles
-distance_str = str(distance)
-
 CARBON_KEY = os.getenv('CARBON_KEY')
-
 url = 'https://www.carboninterface.com/api/v1/estimates'
 
 headers = {
@@ -35,24 +24,43 @@ headers = {
     'Content-Type': 'application/json',
 }
 
-data = {
-    "type": "shipping",
-    "weight_value": int(weightItem),
-    "weight_unit": "lb",
-    "distance_value": distance,
-    "distance_unit": "mi",
-    "transport_method": "truck"
-}
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-response = requests.post(url, headers=headers, data=json.dumps(data))
-carbon_fp = response.json()['data']['attributes']['carbon_lb']
-
-@app.route('/distance', methods=['GET'])
-def get_distance():
-    return jsonify({"distance": distance_str})
-
-@app.route('/carbon_emission',methods=['GET'])
+@app.route('/carbon_emission', methods=['POST'])
 def get_carbon_weight():
-    return jsonify({"carbon": carbon_fp})
 
-app.run(debug=False)
+    print(21)
+    request_data = request.json
+    print(request_data)
+    startDest = geolocator.geocode(request_data['user_address'])
+    endDest = geolocator.geocode(request_data['source_address'])
+    weightItem = request_data['package_weight']
+    print(2)
+
+    startCoords = (startDest.latitude, startDest.longitude)
+    endCoords = (endDest.latitude, endDest.longitude)
+    distance = geodesic(startCoords, endCoords).miles
+    distance_str = str(distance)
+
+    data = {
+        "type": "shipping",
+        "weight_value": int(weightItem),
+        "weight_unit": "lb",
+        "distance_value": distance,
+        "distance_unit": "mi",
+        "transport_method": "truck"
+    }
+    print(3)
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    print(response)
+    carbon_fp = response.json()['data']['attributes']['carbon_lb']
+    print(carbon_fp)
+    # return jsonify({"carbon": carbon_fp})
+
+    # return str(carbon_fp)
+    carbon_lb = 50
+    return str(carbon_lb)
+
+app.run(debug=True)
